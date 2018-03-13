6d9fdaf6bcbbc79e1d2c30b930646054d73f3706
nicolas <n.nuiro@gmail.com>
2018-03-13 11:18:24 +0100

    second_commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express();
 
-const port = process.env.PORT || 3000
 
 app.get('*', (req,res) => {
     res.send('coucou Heroku')
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, (err) => {
 
     if(err){
