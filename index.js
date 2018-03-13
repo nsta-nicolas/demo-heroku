acde6b63aa47961d7f10e53fd690e6d77eaf1ba9
nicolas <n.nuiro@gmail.com>
2018-03-13 11:12:32 +0100

    premier_commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('*', (req,res) => {
+    res.send('coucou Heroku')
+})
+
+app.listen(port, (err) => {
+
+    if(err){
+      return console.log('error connection')
+    }
+    console.log('connect')
+});
