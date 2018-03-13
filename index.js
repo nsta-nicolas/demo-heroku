1294c33a21b2b3c85616c00f9848e4aa9fe200ff
nicolas <n.nuiro@gmail.com>
2018-03-13 12:30:44 +0100

    third_commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
 
+app.use ('/', express.static(__dirname + '/front/dist'));
 
 app.get('*', (req,res) => {
-    res.send('coucou Heroku')
+    res.sendFile(path.join(__dirname, 'front/dist/index.html'))
 })
 
 const port = process.env.PORT || 3000
